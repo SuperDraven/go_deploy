7b3ebd5606fe357680bf226f6eacbf6d9bca0825
lld <375845787@qq.com>
2019-11-08 21:20:43 +0800

    打印webhook数据

--- controllers/Shell.go
+++ controllers/Shell.go
@@ -1,10 +1,15 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"go_deploy/Services"
 )
 
 func Shell(c *gin.Context)  {
+	for k, v :=range c.Request.PostForm {
+		fmt.Printf("k:%v\n", k)
+		fmt.Printf("v:%v\n", v)
+	}
 	Services.ShellGo("git pull")
 }
