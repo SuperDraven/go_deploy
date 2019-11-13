36a351440793fe2b76cfa11243cd6bc7d8234a8e
lld <375845787@qq.com>
2019-11-13 10:12:35 +0800

    修改包名称

--- Conf/index.go
+++ Conf/index.go
@@ -17,7 +17,7 @@ type Config struct {
 }
 
 func LoadConf() *Config {
-	source, errRead := ioutil.ReadFile("./conf/conf.yml")
+	source, errRead := ioutil.ReadFile("./Conf/conf.yml")
 	if errRead != nil {
 		log.Println(errRead)
 	}
